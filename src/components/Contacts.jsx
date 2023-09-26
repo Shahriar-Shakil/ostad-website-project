8c022479c9a3e553ab95a65e4c5813da381a8294
shahriar <amivaloasi>
2023-09-26 22:37:18 +0600

    contact form api added

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,5 +1,43 @@
 "use client";
+
+import toast, { Toaster } from "react-hot-toast";
+import { useState } from "react";
+import axios from "axios";
+
 const Contacts = () => {
+  const [formData, setFormData] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    msg: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const onChange = (data, key) => {
+    setFormData((prev) => {
+      return { ...prev, [key]: data };
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (formData.fname.length === 0) {
+      toast.error("First Name Required");
+    } else if (formData.lname.length === 0) {
+      toast.error("Last Name Required");
+    } else if (formData.email.length === 0) {
+      toast.error("Email Required");
+    } else if (formData.msg.length === 0) {
+      toast.error("Message Required");
+    }
+    try {
+      const rawResponse = await axios.post("/api/contact", formData);
+      toast.success("Your Message Sent Successfully");
+    } catch (error) {
+      toast.success("something Bad happen");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <section className="py-20 bg-gray-50">
       <div className="container mx-auto px-4">
@@ -33,7 +71,7 @@ const Contacts = () => {
           <div className="w-full lg:w-1/2 px-4">
             <div className="max-w-sm mx-auto lg:mr-0 lg:ml-auto">
               <div className="mb-6 py-8 px-6 bg-white shadow rounded-t-3xl rounded-bl-3xl text-center">
-                <form action="">
+                <form onSubmit={handleSubmit}>
                   <div className="mb-6">
                     <span className="text-sm text-gray-400">Sign Up</span>
                     <h4 className="text-2xl">Create an account</h4>
@@ -44,6 +82,7 @@ const Contacts = () => {
                         className="py-2 px-3 w-full bg-gray-50 rounded leading-loose"
                         type="text"
                         placeholder="First Name"
+                        onChange={(e) => onChange(e.target.value, "fname")}
                       />
                     </div>
                     <div className="w-full lg:w-1/2 px-2">
@@ -51,6 +90,7 @@ const Contacts = () => {
                         className="py-2 px-3 w-full bg-gray-50 rounded leading-loose"
                         type="text"
                         placeholder="Last Name"
+                        onChange={(e) => onChange(e.target.value, "lname")}
                       />
                     </div>
                   </div>
@@ -58,15 +98,22 @@ const Contacts = () => {
                     className="mb-4 py-2 px-3 w-full bg-gray-50 rounded leading-loose"
                     type="email"
                     placeholder="hello@example.com"
+                    onChange={(e) => onChange(e.target.value, "email")}
                   />
                   <textarea
                     className="mb-4 py-2 px-3 w-full bg-gray-50 rounded leading-loose"
                     type="email"
                     placeholder="Message"
+                    onChange={(e) => onChange(e.target.value, "msg")}
                   />
 
-                  <button className="mb-4 py-4 w-full rounded text-sm bg-green-600 hover:bg-green-700 text-white font-bold leading-normal transition duration-200">
-                    Send
+                  <button
+                    disabled={loading}
+                    className={`${
+                      loading ? "bg-green-600/30" : "bg-green-600"
+                    } mb-4 py-4 w-full rounded text-sm  hover:bg-green-700 text-white font-bold leading-normal transition duration-200`}
+                  >
+                    {loading ? "Sending" : "Send"}
                   </button>
                 </form>
               </div>
@@ -74,6 +121,7 @@ const Contacts = () => {
           </div>
         </div>
       </div>
+      <Toaster position="bottom-center" />
     </section>
   );
 };
